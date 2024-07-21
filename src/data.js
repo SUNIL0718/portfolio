35f942617df252903efc69c8b336ef531ec4d6b8
SUNIL0718 <skay0718@gmail.com>
2024-07-21 12:01:49 +0530

    css update

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
   {
     id: 1,
     img: require("./img/one.png"),
-    link: "http://lama.dev",
+    link: "https://sunil0718.github.io",
   },
   {
     id: 2,
     img: require("./img/two.png"),
-    link: "http://lama.dev",
+    link: "https://sunil0718.github.io",
   },
   {
     id: 3,
     img: require("./img/three.png"),
-    link: "http://lama.dev",
+    link: "https://sunil0718.github.io",
   },
   {
     id: 4,
     img: require("./img/four.png"),
-    link: "http://lama.dev",
+    link: "https://sunil0718.github.io",
   },
   {
     id: 5,
     img: require("./img/five.png"),
-    link: "http://lama.dev",
+    link: "https://sunil0718.github.io",
   },
   {
     id: 6,
     img: require("./img/six.png"),
-    link: "http://lama.dev",
+    link: "https://sunil0718.github.io",
   },
 ];
